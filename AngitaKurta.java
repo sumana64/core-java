f141b0b0bb2077c539249f684c967e44f4b16ed4
sumana64 <sumanahr.xworkz@gmail.com>
2022-09-22 17:40:42 +0530

    committing java files

---
+++ AngitaKurta.java
@@ -0,0 +1,29 @@
+class AngitaKurta{
+          
+		  		   public static void main(String sum[]){
+
+                   int    price  = 700;
+                   String idealFor = "Women";
+				   String lengthType = "Calf Length";
+				   String brandColor = "LAVENDER";
+				   String occasion   = "Casual";
+                   String pattern    = "Chikan Embroidery";
+							  
+				   System.out.println("price is " + price);		  
+				   System.out.println("idealfor is " + price);		  
+                   System.out.println("lengthType is" + lengthType);
+				   System.out.println("occasion is" + occasion);
+				   System.out.println("pattern is" + pattern);
+				   System.out.println("brandColor is" + brandColor);
+
+
+
+
+
+
+
+
+    }
+
+
+}
---
+++ Biscuit.java
@@ -0,0 +1,21 @@
+class Biscuit{
+
+       public static void main(String sun[]){
+	   
+	           String biscuitName = "oreo";
+			   int    biscuitPrice = 10;
+			   System.out.println("biscuitname is " + biscuitName);
+               System.out.println("biscuitprice is " + biscuitPrice);			   
+			  
+	   
+	   
+	   
+	   
+	   
+	   }
+
+
+
+
+
+}
